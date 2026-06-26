5b79526a058f5ff3f939a00b240300c263038a4a
vignesh3327 <99711217+vignesh3327@users.noreply.github.com>
2026-06-26 10:58:06 +0530

    Rename variable 'buckett' to 'bucket'

--- variables.tf
+++ variables.tf
@@ -106,7 +106,7 @@ variable "region" {
   default     = null
 }
 
-variable "buckett" {
+variable "bucket" {
   description = "(Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name."
   type        = string
   default     = null
